7b9253a65156d23f8aac2a100197b760c3ef1187
Kristjan Lääne <xonproject@gmail.com>
2024-10-26 16:50:19 +0300

    action

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -3,8 +3,14 @@
 import prisma from '@/lib/db';
 import { Pet } from '@/lib/types';
 
-export async function addPet(pet) {
+export async function addPet(formData) {
   await prisma.pet.create({
-    data: pet,
+    data: {
+      name: formData.get('name'),
+      ownerName: formData.get('ownerName'),
+      age: parseInt(formData.get('age')),
+      imageUrl: formData.get('imageUrl'),
+      notes: formData.get('notes'),
+    },
   });
 }
