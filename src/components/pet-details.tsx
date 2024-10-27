95792dd7f5e577ae0d426c6b4f0a6f72f985a927
Kristjan Lääne <xonproject@gmail.com>
2024-10-27 11:12:06 +0200

    checkoutpet server action

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -4,6 +4,7 @@ import { usePetContext } from '@/lib/hooks';
 import { Pet } from '@/lib/types';
 import Image from 'next/image';
 import PetButton from './pet-button';
+import { deletePet } from '@/actions/actions';
 
 export default function PetDetails() {
   const { selectedPet } = usePetContext();
@@ -57,7 +58,7 @@ function TopBar({ pet }: Props) {
 
         <PetButton
           actionType='checkout'
-          onClick={() => handleCheckoutPet(pet.id)}
+          onClick={async () => await deletePet(pet.id)}
         >
           Checkout
         </PetButton>
