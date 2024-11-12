74b8226fc1e097df39bc676bff67f8f4765746b7
Kristjan Lääne <xonproject@gmail.com>
2024-11-12 17:21:05 +0200

    next-auth config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
-import { NextResponse } from 'next/server';
+// export function middleware(request: Request) {
+//   return NextResponse.next();
+// }
 
-export function middleware(request: Request) {
-  return NextResponse.next();
-}
+import { auth } from './lib/auth';
+
+export default auth;
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
