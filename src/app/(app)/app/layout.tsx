884d890457f41c968b621d0154b1d7aefc8c5f4d
Kristjan Lääne <xonproject@gmail.com>
2024-11-14 17:53:47 +0200

    server-only

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -5,8 +5,6 @@ import { Toaster } from '@/components/ui/sonner';
 import PetContextProvider from '@/contexts/pet-context-provider';
 import SearchContextProvider from '@/contexts/search-context-provider';
 import prisma from '@/lib/db';
-import { auth } from '@/lib/auth';
-import { redirect } from 'next/navigation';
 import { checkAuth } from '@/lib/server-utils';
 
 export default async function Layout({
