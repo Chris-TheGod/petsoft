ba1ee300f29612ea835fc2d8d3b3d894332b9527
Kristjan Lääne <xonproject@gmail.com>
2024-11-17 12:51:12 +0200

    canceled payment searchparams

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -24,6 +24,12 @@ export default function Page({ searchParams }) {
           Payment successful! You now have lifetime access to PetSoft!
         </p>
       )}
+
+      {searchParams.cancelled && (
+        <p className='text-sm text-red-700'>
+          Payment canceled. You can try again.
+        </p>
+      )}
     </main>
   );
 }
